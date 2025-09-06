f679b73c27137f24b49d570a3afb112973a38346
Huzaifa <huzaifa4371@gmail.com>
2025-09-06 10:19:21 +0530

    Api created to get Number of wheels from database

--- index.js
+++ index.js
@@ -7,18 +7,34 @@ const app = express();
 app.use(cors());
 app.use(body_parser.json());
 
+let database;
 try {
-  const database = mysql.createConnection({
+  database = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "Pass@123",
     database: "vehiclebooking",
-  });
+  }).promise();
 
   console.log("Server Connected");
 } catch (error) {
   console.log(`Error : ${error}`);
 }
+
+// Api created to get Number of wheels from database
+app.get("/vehicleinfo/:wheels", async (req, res) => {
+  try {
+    const wheels = req.params.wheels;
+    const [rows] = await database.query(
+      "SELECT * FROM vehicleType where wheels = ? ",
+      [wheels]
+    );
+    // console.log(wheels);
+    res.send(rows);  
+  } catch (error) {
+    res.send({"Error" : error});
+  }
+});
 
 app.listen(3000, () => {
   console.log("server active at port 3000");
